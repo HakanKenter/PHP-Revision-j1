425e50f21dadc08cfa9a119014b15cfb6afb4d14
Hakan Kenter <hakan.kt@outlook.fr>
2021-11-16 16:54:44 +0100

    Ajout dossie site update

--- site/about.php
+++ site/about.php
@@ -1,11 +1,11 @@
+<?php
+    $page_name = "About";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href='style.css'>
-    <title>Document</title>
+    <?php include("partials/head.php"); ?>
 </head>
 <body>
 
